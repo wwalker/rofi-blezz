7dd61e3c4fabef824702988120c152fe33cbbab5
Wayne Walker <wwalker@solid-constructs.com>
2025-06-11 19:38:00 -0500

    fix for #5 - allow multiple characters

--- src/blezz.c
+++ src/blezz.c
@@ -143,13 +143,7 @@ static Node *blezz_parse_dir_node(char *start) {
   if (strv && strv[0] && strv[1]) {
     node = g_malloc0(sizeof(Node));
     node->type = DIR_REF;
-    node->icon = g_strdup("folder");
-    char buf[6] = {
-        0,
-    };
-    gunichar uchar = g_utf8_get_char(strv[0]);
-    g_unichar_to_utf8(uchar, &(buf[0]));
-    node->hotkey = g_strdup(buf);
+    node->hotkey = g_strstrip(g_utf8_strdown(strv[0], -1));
     node->name = g_strstrip(g_strdup(strv[1]));
     if (strv[2] != NULL) {
       node->icon = g_strstrip(g_strdup(strv[2]));
@@ -164,12 +158,7 @@ static Node *blezz_parse_act_node(char *start, gboolean reload) {
   if (strv && strv[0] && strv[1] && strv[2]) {
     node = g_malloc0(sizeof(Node));
     node->type = ACT_REF;
-   char buf[6] = {
-        0,
-    };
-    gunichar uchar = g_utf8_get_char(strv[0]);
-    g_unichar_to_utf8(uchar, &(buf[0]));
-    node->hotkey = g_strdup(buf);
+    node->hotkey = g_utf8_strdown(strv[0], -1);
     node->name = g_strdup(strv[1]);
     node->command = g_strdup(strv[2]);
     if (strv[3] != NULL) {
